db644ef4bd2bb67d15e199fb3eb15491a913c2f2
Fdasert <fdasert434@gmail.com>
2023-09-23 18:26:40 +0300

    Дизайн сайта

---
+++ shop/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>BroykShop</span>
+            <ul className='nav'>
+                <il>Доставка</il>
+                <il>О нас</il>
+                <il>Контакты</il>
+            </ul>
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
