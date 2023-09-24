0792ea1eefca8f742763e2445e6bfcffd876dea6
Fdasert <fdasert434@gmail.com>
2023-09-24 20:21:59 +0300

    Логика корзины + стили для товаров

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingBasket } from "react-icons/fa";
+import Orderjs from './Order';
 
-export default function header() {
+const showOrders = (props) => {
+  return (<div>
+    {props.orders.map(el => (
+        <Orderjs key={el.id} item={el}/>
+  ))}
+  </div>)
+}
+
+
+const showNothing = () => {
+  return(<div className='empty'>
+    <h2>Корзина пуста</h2>
+  </div>)
+}
+
+
+export default function Header(props) {
+  let [cartOpen, SetCartOpen] = useState(false);
+
   return (
     <header>
         <div>
@@ -10,6 +30,15 @@ export default function header() {
                 <il>О нас</il>
                 <il>Контакты</il>
             </ul>
+            <FaShoppingBasket onClick={() => SetCartOpen(cartOpen = !cartOpen)} className={`shop-button ${cartOpen && 'active'}`}/>
+
+
+            {cartOpen && (
+              <div className='shop-cart'>
+                {props.orders.length > 0 ? 
+                  showOrders(props) : showNothing()}
+              </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
