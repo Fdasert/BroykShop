28caefb0acf152bda9f798ee93f630051e49c528
Fdasert <fdasert434@gmail.com>
2023-09-25 19:52:23 +0300

    Модальные окна

--- shop/src/components/Order.js
+++ shop/src/components/Order.js
@@ -5,7 +5,7 @@ export class Orderjs extends Component {
   render() {
     return (
       <div className='item'>
-            <img src={"./img/" + this.props.item.img} />
+            <img alt="" src={"./img/" + this.props.item.img} />
             <h2>{this.props.item.title}</h2>
             <b>{this.props.item.price}₽</b>
             <FaTrash className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)} />
